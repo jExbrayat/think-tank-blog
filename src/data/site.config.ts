00da96b2ebba77afb1ea6bcfeb7a5d0cd09100b9
jEx <jules.exbrayat@ensimag-inp.org>
2025-02-23 22:15:05 +0100

    add repo name as base

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -13,7 +13,7 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	site: 'https://jexbrayat.github.io/think-tank-blog/', // Write here your website url
-	base: '',
+	base: '/think-tank-blog/',
 	author: 'Jules Exbrayat', // Site author
 	title: "IA de l'Espoir", // Site title.
 	subTitle: 'French Think-Tank, World Wide',
