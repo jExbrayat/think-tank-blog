ecadf43509957e31e5a626ee00ac2adc54607147
jEx <122873708+jExbrayat@users.noreply.github.com>
2025-02-23 19:25:44 +0100

    Update site.config.ts

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -11,7 +11,7 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	site: 'https://jexbrayat.github.io/', // Write here your website url
+	site: 'https://jexbrayat.github.io/think-tank-blog/', // Write here your website url
 	author: 'Jules Exbrayat', // Site author
 	title: "IA de l'Espoir", // Site title.
 	subTitle: 'French Think-Tank, World Wide',
